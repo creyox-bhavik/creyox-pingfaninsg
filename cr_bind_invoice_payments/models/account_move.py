86a0d4d8a917c1f1a132388f689adc341c279a29
creyox-bhavik <bhavik.dhabaliya@creyox.com>
2023-11-13 12:18:38 +0530

    sequence changes

--- cr_bind_invoice_payments/models/account_move.py
+++ cr_bind_invoice_payments/models/account_move.py
@@ -6,3 +6,22 @@ class AccountMove(models.Model):
     _inherit = 'account.move'
 
     old_name = fields.Char()
+
+    def _get_starting_sequence(self):
+        # EXTENDS account sequence.mixin
+        self.ensure_one()
+        is_payment = self.payment_id or self._context.get('is_payment')
+        if self.journal_id.type == 'sale':
+            starting_sequence = "%s/%04d/00000" % (
+                self.journal_id.code, self.date.year)
+        elif self.journal_id.type in ['bank', 'cash']:
+            starting_sequence = "%s%04d%02d-0000" % (
+                self.journal_id.code, self.date.year, self.date.month)
+        else:
+            starting_sequence = "%s/%04d/%02d/0000" % (
+                self.journal_id.code, self.date.year, self.date.month)
+        if self.journal_id.refund_sequence and self.move_type in ('out_refund', 'in_refund'):
+            starting_sequence = "R" + starting_sequence
+        # if self.journal_id.payment_sequence and is_payment:
+        #     starting_sequence = "P" + starting_sequence
+        return starting_sequence
