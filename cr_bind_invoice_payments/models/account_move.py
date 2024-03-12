9c1f3bc34004056449516fa28e3d53614dab60b1
creyox-odoo <creyoxtech@gmail.com>
2024-03-12 17:49:41 +0530

    default code added

--- cr_bind_invoice_payments/models/account_move.py
+++ cr_bind_invoice_payments/models/account_move.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields,api
-import logging
+from odoo import models, fields
 
-_logger = logging.getLogger(__name__)
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
@@ -27,59 +25,3 @@ class AccountMove(models.Model):
         # if self.journal_id.payment_sequence and is_payment:
         #     starting_sequence = "P" + starting_sequence
         return starting_sequence
-
-    def write(self,vals):
-        _logger.info("\n\n 17 %s",vals)        
-
-        if 'name' in vals:
-
-            _logger.info("\n\n 36 %s",vals)        
-            del vals['name']
-            _logger.info("\n\n 38 %s",vals)        
-            res = super().write(vals)
-            
-            for rec in self:
-                rec.payment_id.name = rec.payment_id.old_name
-            return res
-
-        else:
-            res = super().write(vals)
-            return res
-    
-
-    
-    def _inverse_name(self):
-        self._conditional_add_to_compute('payment_reference', lambda move: (
-            move.name and move.name != '/'
-        ))
-
-        _logger.info("\n\n 54 %s",self)        
-    
-
-    @api.depends('posted_before', 'state', 'journal_id', 'date')
-    def _compute_name(self):
-        self = self.sorted(lambda m: (m.date, m.ref or '', m.id))
-
-        for move in self:
-            move_has_name = move.name and move.name != '/'
-            if move_has_name or move.state != 'posted':
-                if not move.posted_before and not move._sequence_matches_date():
-                    if move._get_last_sequence(lock=False):
-                        # The name does not match the date and the move is not the first in the period:
-                        # Reset to draft
-                        move.name = False
-                        continue
-                else:
-                    if move_has_name and move.posted_before or not move_has_name and move._get_last_sequence(lock=False):
-                        # The move either
-                        # - has a name and was posted before, or
-                        # - doesn't have a name, but is not the first in the period
-                        # so we don't recompute the name
-                        continue
-            if move.date and (not move_has_name or not move._sequence_matches_date()):
-                move._set_next_sequence()
-
-        self.filtered(lambda m: not m.name and not move.quick_edit_mode).name = '/'
-        self._inverse_name()
-
-        _logger.info("\n\n 83 %s",self)        
--- cr_bind_invoice_payments/models/account_payment.py
+++ cr_bind_invoice_payments/models/account_payment.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import models, fields, api
-import logging
 
-_logger = logging.getLogger(__name__)
 
 class AccountPayment(models.Model):
     _inherit = 'account.payment'
@@ -17,49 +15,3 @@ class AccountPayment(models.Model):
         res.sudo().name = '/'
         res.sudo().move_id._compute_name()
         return res
-
-    # def _synchronize_to_moves(self, changed_fields):
-    #     _logger.info("\n\n 22")        
-    
-
-    def write(self,vals):
-        _logger.info("\n\n 26 %s", vals)        
-        res = super().write(vals)
-
-        return res
-
-    def _inverse_name(self):
-        self._conditional_add_to_compute('payment_reference', lambda move: (
-            move.name and move.name != '/'
-        ))
-
-        _logger.info("\n\n 54 %s",self)        
-    
-
-    @api.depends('posted_before', 'state', 'journal_id', 'date')
-    def _compute_name(self):
-        self = self.sorted(lambda m: (m.date, m.ref or '', m.id))
-
-        for move in self:
-            move_has_name = move.name and move.name != '/'
-            if move_has_name or move.state != 'posted':
-                if not move.posted_before and not move._sequence_matches_date():
-                    if move._get_last_sequence(lock=False):
-                        # The name does not match the date and the move is not the first in the period:
-                        # Reset to draft
-                        move.name = False
-                        continue
-                else:
-                    if move_has_name and move.posted_before or not move_has_name and move._get_last_sequence(lock=False):
-                        # The move either
-                        # - has a name and was posted before, or
-                        # - doesn't have a name, but is not the first in the period
-                        # so we don't recompute the name
-                        continue
-            if move.date and (not move_has_name or not move._sequence_matches_date()):
-                move._set_next_sequence()
-
-        self.filtered(lambda m: not m.name and not move.quick_edit_mode).name = '/'
-        self._inverse_name()
-
-        _logger.info("\n\n 83 %s",self)        
